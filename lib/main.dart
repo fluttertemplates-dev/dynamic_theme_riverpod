fa5e7e469122e453d143bb4757c86b48277f8990
Bibek Timsina <timsinabibek960@gmail.com>
2021-09-03 17:10:25 +0545

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  runApp(ProviderScope(child: MyApp()));
+}
+
+class ThemeNotifier extends ChangeNotifier {
+  // Define your default thememode here
+  ThemeMode themeMode = ThemeMode.light;
+  SharedPreferences? prefs;
+
+  ThemeNotifier() {
+    _init();
+  }
+
+  _init() async {
+    // Get the stored theme from shared preferences
+    prefs = await SharedPreferences.getInstance();
+
+    int _theme = prefs?.getInt("theme") ?? themeMode.index;
+    themeMode = ThemeMode.values[_theme];
+    notifyListeners();
+  }
+
+  setTheme(ThemeMode mode) {
+    themeMode = mode;
+    notifyListeners();
+    // Save the selected theme using shared preferences
+    prefs?.setInt("theme", mode.index);
+  }
+}
+
+final themeNotifierProvider =
+    ChangeNotifierProvider<ThemeNotifier>((_) => ThemeNotifier());
+
+class MyApp extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    final _themeNotifier = watch(themeNotifierProvider);
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      themeMode: _themeNotifier.themeMode,
+      // Define your themes here
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends ConsumerWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    final notifier = watch(themeNotifierProvider);
+
+    return Scaffold(
+        appBar: AppBar(title: Text("Dynamic Theme")),
+        body: Column(
+          children: [
+            _singleTile("Dark Theme", ThemeMode.dark, notifier),
+            _singleTile("Light Theme", ThemeMode.light, notifier),
+            _singleTile("System Theme", ThemeMode.system, notifier),
+          ],
+        ));
+  }
+
+  Widget _singleTile(String title, ThemeMode mode, ThemeNotifier notifier) {
+    return RadioListTile<ThemeMode>(
+        value: mode,
+        title: Text(title),
+        groupValue: notifier.themeMode,
+        onChanged: (val) {
+          if (val != null) notifier.setTheme(val);
+        });
+  }
+}
